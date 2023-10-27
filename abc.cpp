a35210fd7e827ca3f8ece518d16f51238d8f61ef
Yugal Gaming PC <22d1412@iitb.ac.in>
2023-10-28 00:44:22 +0530

    latest

---
+++ abc.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+   // printf() displays the string inside quotation
+   int x;
+   cin>>x;
+   if(x){
+    cout<<"hello";
+   }
+   return 0;
+}
