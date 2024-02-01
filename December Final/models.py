fbe98dd71c71a7a23e2705eef882af1338740fa2
Yugal0 <yugal2531@gmail.com>
2024-02-01 10:43:16 +0530

    latest

--- December Final/models.py
+++ December Final/models.py
@@ -3,6 +3,7 @@ from impedance.models.circuits import CustomCircuit
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
+from mpmath import *
 
 @element(num_params=3, units=["Ohm", "F", ""],overwrite=True)
 def TLMuni(p,f):
@@ -66,6 +67,30 @@ def TLMlin(p,f):
     kk2=-2/3*(t12*S/(t12-1))
     Z = (2*R1/S)*(scipy.special.iv(1/3, kk1)*scipy.special.iv(2/3, kk2)-scipy.special.iv(-1/3, kk1)*scipy.special.iv(-2/3, kk2))/((scipy.special.iv(-1/3, kk1))*scipy.special.iv(1/3, kk2)-scipy.special.iv(1/3, kk1)*scipy.special.iv(-1/3, kk2))
     return Z
+
+@element(num_params=4, units=["Ohm", "Ohm F", "Ohm F", ""],overwrite=True)
+def TLMlinnew(p,f):
+    omega = 2 * np.pi * np.array(f)
+    R1, R1Q, R2Q, alpha = p[0], p[1], p[2], p[3]
+    Q=R1Q/R1
+    R2=R2Q/Q
+    t12=R1/R2
+    S=np.sqrt((1j*omega)**alpha*R1*Q)
+    kk1=-2/3*(S*(t12**(-1/2)/(t12-1)))
+    kk2=-2/3*(t12*S/(t12-1))
+    Z=[]
+    for omg in omega:
+        S=np.sqrt((1j*omg)**alpha*R1*Q)
+        kk1=-2/3*(S*(t12**(-1/2)/(t12-1)))
+        kk2=-2/3*(t12*S/(t12-1))
+        Zelem = (2*R1/S)*(mp.besseli(1/3, kk1)*mp.besseli(2/3, kk2)-mp.besseli(-1/3, kk1)*mp.besseli(-2/3, kk2))/((mp.besseli(-1/3, kk1))*mp.besseli(1/3, kk2)-mp.besseli(1/3, kk1)*mp.besseli(-1/3, kk2))
+        Zelem=mp.nstr(Zelem,n=50)
+        Zelem = Zelem.replace(' ','').replace('(','').replace(')','') 
+        Z.append(Zelem)
+
+    Z=np.asarray(Z,dtype=complex)
+    return Z
+
 
 @element(num_params=4, units=["Ohm", "Ohm F", "Ohm F", ""],overwrite=True)
 def TLMlinzert(p,f):
